7afcef408cb6a34fe9d4202550ead048882fdb9c
Dianna Tingg <dtingg@gmail.com>
2019-08-22 08:25:00 -0700

    all the tests are passing

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -21,8 +21,7 @@ def total_medals_per_country(olympic_data)
       else
         total_medals_hash[team] = 1
       end
-    end
-    
+    end    
   end
   
   total_medals_array = total_medals_hash.map do |key, value|
@@ -47,52 +46,33 @@ def all_gold_medal_winners(olympic_data)
   gold_winners = olympic_data.select do |row_hash|
     row_hash["Medal"] == "Gold"
   end
-  
   return gold_winners
 end
 
-# returns an array of hashes, each containing all the same data as the input but containing only Gold medal winners.
-
-
-
-#   olympic_data.each do |row_hash|
-#   
-#     if medal != "NA"
-#       if total_medals_hash.has_key?(team)
-#         total_medals_hash[team] += 1
-#       else
-#         total_medals_hash[team] = 1
-#       end
-#     end
-
-#   end
-
-#   total_medals_array = total_medals_hash.map do |key, value|
-#     {"country"=>key, "count"=>value}
-#   end
-
-#   return total_medals_array
-# end
-
-
-
-
-
-
-
-
-
-
-
-
 def medals_sorted_by_country(medal_totals)
-  
+  sorted = medal_totals.sort_by do |hash|
+    hash["country"]
+  end
+  return sorted
 end
 
 def country_with_most_medals(medal_totals)
-  
+  sorted = medal_totals.max_by do |hash|
+    hash["count"]
+  end
+  return sorted
 end
 
 def athlete_height_in_inches(olympic_data)
+  new_data = []
   
+  olympic_data.each do |hash|
+    new_height = hash["Height"].to_f / 2.5
+    
+    new_hash = hash.clone
+    new_hash["Height"] = new_height
+    new_data.push(new_hash)
+  end
+  
+  return new_data
 end
